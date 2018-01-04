349e88fe1a9dcb39bf5090f0dd520a6c931e3148
Cindy Juarez <sceendy@gmail.com>
2018-01-04 11:06:23 -0600

    added: pipe to list posts in order from most recent

--- src/app/pipes/blogOrder.filter.ts
+++
@@ -1,9 +0,0 @@
-import { Pipe, PipeTransform } from '@angular/core';
-
-@Pipe({ name: 'blogFilter' })
-
-export class BlogFilterPipe implements PipeTransform {
-  transform(posts: any): any {
-    console.log(posts);
-  }
-}
